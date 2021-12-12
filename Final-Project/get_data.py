99fe5de044afa02d5f6eab5908693c0846456ee1
Ummuhan <ummuhandemir807@gmail.com>
2021-12-12 16:21:46 +0100

    Final project added

---
+++ Final-Project/get_data.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+from random import randint
+import pandas as pd
+import os
+import json
+import tqdm
+
+urls = pd.read_csv('coursera.csv',delimiter = ';')
+url_list = list(urls['link-href'])
+
+for ix,url in enumerate(tqdm.tqdm(url_list[1264:]),start=1264):
+    response = requests.get(url)
+    #print(response.status_code)
+#     pickle.dump(str(response.content,'utf-8'), open('./raw_data/{}.pkl'.format(str(ix)), 'w'))
+    with open('./raw_data_txt/{}.txt'.format(str(ix)), 'wb') as f:
+        f.write(response.content)
+    wait_time=randint(1,3)
+    sleep(wait_time)
